49b8acbd42bd2e66cda5bdf70b5f428b3a291693
tamtamstrong <gogolan.adrian@gmail.com>
2015-01-25 19:06:40 +0200

    change handled keyboard event

--- pong.js
+++ pong.js
@@ -1,33 +1,33 @@
 var leftPlayer = { 
-   upKey: 85,
-   downKey: 74,
+   upKey: 85, // U
+   downKey: 74, // J
    bat : undefined
 };
 
 var rightPlayer = {
-   upKey: 38,
-   downKey: 40,
+   upKey: 38,	// arrow up
+   downKey: 40,	// arrow down
    bat : undefined
 };
 
 leftPlayer.bat = document.getElementById("leftPlayer");
 rightPlayer.bat = document.getElementById("rightPlayer");
 
-var handleKeyPress = function (args) {
+var handleKeyDown = function (args) {
    switch (args.keyCode) {
       case leftPlayer.upKey:
          leftPlayer.bat.style.top = 200;
          break;
       case leftPlayer.downKey:
-         
+         leftPlayer.bat.style.top = 400;
          break;
       case rightPlayer.upKey:
-      
+      	rightPlayer.bat.style.top = 200;
          break;
       case rightPlayer.downKey:
-      
+      	rightPlayer.bat.style.top = 400;
          break;
    }   
 }
 
-document.addEventListener("keydown", handleKeyPress);
+document.addEventListener("keydown", handleKeyDown);

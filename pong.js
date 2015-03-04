3cabada0842924cd72ebf1d7c3beecde5561d4c0
tamtamstrong <gogolan.adrian@gmail.com>
2015-03-04 23:06:59 +0200

    Build the Pong namespace containing gamestate objects

--- pong.js
+++ pong.js
@@ -1,33 +1,46 @@
-var leftPlayer = { 
-   upKey: 85, // U
-   downKey: 74, // J
-   bat : undefined
-};
+var Pong = Pong || {};
 
-var rightPlayer = {
-   upKey: 38,	// arrow up
-   downKey: 40,	// arrow down
-   bat : undefined
-};
-
-leftPlayer.bat = document.getElementById("leftPlayer");
-rightPlayer.bat = document.getElementById("rightPlayer");
-
-var handleKeyDown = function (args) {
-   switch (args.keyCode) {
-      case leftPlayer.upKey:
-         leftPlayer.bat.style.top = 200;
-         break;
-      case leftPlayer.downKey:
-         leftPlayer.bat.style.top = 400;
-         break;
-      case rightPlayer.upKey:
-      	rightPlayer.bat.style.top = 200;
-         break;
-      case rightPlayer.downKey:
-      	rightPlayer.bat.style.top = 400;
-         break;
-   }   
+Pong.SquareSize = function(width, height) {
+ this.width = width,
+ this.height = height
 }
 
-document.addEventListener("keydown", handleKeyDown);
+Pong.Table = function(width, height) {
+ this.size = new Pong.SquareSize(width, height);
+}
+
+Pong.Position = function(x, y) {
+ this.x = x;
+ this.y = y;
+}
+
+Pong.Ball = function(x, y, r, speed) {
+ this.position = new Pong.Position(x, y);
+ this.radius = r;
+ this.speed = speed;
+}
+
+Pong.Bat = function(x, y, width, height, speed) {
+ this.Position =  new Pong.Position(x, y);
+ this.Size = new Pong.SquareSize(width, height);
+ this.speed = speed;
+}
+
+Pong.Game = function(tablewidth, tableheight, speed) {
+ this.tablewidth = tablewidth;
+ this.tableheight = tableheight;
+ this.batwidth = tablewidth / 20;
+ this.batheight = tableheight / 20;
+ this.ballradius = tablewidth / 30;
+ this.speed = speed;
+
+ this.bats = { 
+  left: new Pong.Bat(0, 0, this.batwidth, this.batheight, this/speed),
+  right: new Pong.Bat(this.tablewidth - this.batwidth, this.tableheight - this.batheight, this.batwidth, this.batheight, this.speed)
+ }
+ 
+ this.table = new Pong.Table(this.tablewidth, this.tableheight);
+ 
+ this.ball = new Pong.Ball(this.tablewidth / 2, this.tableheight / 2, this.ballradius, this.speed);
+
+}
